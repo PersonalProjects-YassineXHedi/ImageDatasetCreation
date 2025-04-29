4d59f322748ba820dd76a2ee1ce8efcb01462873
Yassine SAHLI <ysahl091@uottawa.ca>
2025-04-28 22:37:24 -0400

    webscrapping and image review

---
+++ main.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from web_scrapper import get_images_from_google
+from reviewer import review_images
+
+if __name__ == "__main__":
+    # Setup ChromeDriver
+    PATH = "C:/Users/yassi/Desktop/Projet/CookBotProject/WebScrappingDownloads/chromedriver.exe" 
+    service = Service(executable_path=PATH)
+    wd = webdriver.Chrome(service=service)
+
+    # Scrape images
+    urls = get_images_from_google(wd, delay=1, max_images=5)
+    wd.quit()
+
+    # Review and selectively download
+    review_images(urls, save_folder="imgs/")
