402e44bed70bf85049021ce4e9e0848f6cfa8fd4
Yassine SAHLI <ysahl091@uottawa.ca>
2025-05-02 17:32:34 -0400

    added the possibility to search on google shoppings

--- web_scrapper.py
+++ web_scrapper.py
@@ -5,7 +5,16 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 
 
-def get_images_from_google(query, wd, delay, max_images):
+def get_images_from_google(search_type, query, wd, delay, max_images):
+    match search_type:
+        case 'images':
+            get_images_from_google_images(query, wd, delay, max_images)
+        case 'shoppings':
+            get_images_from_google_shop(query, wd, delay, max_images)
+
+
+
+def get_images_from_google_images(query, wd, delay, max_images):
     def scroll_down(wd):
         wd.execute_script("window.scrollBy(0, document.body.scrollHeight);")
         time.sleep(delay)
@@ -34,7 +43,7 @@ def get_images_from_google(query, wd, delay, max_images):
             last_index += 1
             try:
                 wd.execute_script("arguments[0].scrollIntoView({behavior: 'instant', block: 'center', inline: 'center'});", thumb)
-
+                time.sleep(delay/2)
                 if thumb.is_displayed() and thumb.is_enabled():
                     actions = ActionChains(wd)
                     actions.move_to_element(thumb).pause(0.3).click().perform()
@@ -81,3 +90,78 @@ def get_images_from_google(query, wd, delay, max_images):
         
     return list(image_urls)
 
+def get_images_from_google_shop(query, wd, delay, max_images):
+    def scroll_down(wd):
+        wd.execute_script("window.scrollBy(0, document.body.scrollHeight);")
+        time.sleep(delay)
+
+    url = f"https://www.google.com/search?q={query}&tbm=shop"
+    wd.get(url)
+
+    image_urls = set()
+    skips = 0
+    last_index = 0
+
+    while len(image_urls) + skips < max_images:
+        try:
+            WebDriverWait(wd, 10).until(
+                EC.presence_of_all_elements_located((By.CLASS_NAME, "njFjte"))
+            )
+            thumbnails = wd.find_elements(By.CLASS_NAME, "njFjte")
+        except Exception as e:
+            print(e)
+            skips += 1
+            max_images += 1
+            continue
+        for thumb in thumbnails[last_index:]:
+            if(len(image_urls) + skips >= max_images):
+                break
+            last_index += 1
+            try:
+                wd.execute_script("arguments[0].scrollIntoView({behavior: 'instant', block: 'center', inline: 'center'});", thumb)
+                time.sleep(delay/2)
+                if thumb.is_displayed() and thumb.is_enabled():
+                    actions = ActionChains(wd)
+                    actions.move_to_element(thumb).pause(0.3).click().perform()
+                    time.sleep(delay)
+                else:
+                    skips += 1
+                    max_images += 1
+                    continue
+
+            except Exception as e:
+                print(e)
+                skips += 1
+                max_images += 1
+                continue
+            
+            try:
+                WebDriverWait(wd, 10).until(
+                    EC.presence_of_all_elements_located((By.CLASS_NAME, "KfAt4d"))
+                )
+            except Exception as e:
+                print(e)
+                skips += 1
+                max_images += 1
+                continue
+            images = wd.find_elements(By.CLASS_NAME, "KfAt4d")
+            
+            found_valid_image = False
+            for image in images:
+                if image.get_attribute('src') in image_urls:
+                    max_images += 1
+                    skips += 1
+                    break
+
+                if image.get_attribute('src') and 'http' in image.get_attribute('src'):
+                    image_urls.add(image.get_attribute('src'))
+                    print(f"Found {len(image_urls)}")
+                    found_valid_image = True
+            if not found_valid_image:
+                skips += 1
+                max_images += 1
+                continue
+        scroll_down(wd)
+
+        
+    return list(image_urls)
